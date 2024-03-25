830d63f17d99c02cce074e0a82e4af56c6b09589
Florian Spindler <florian.spindler@etu.unistra.fr>
2024-03-25 22:23:27 +0100

    CalculMensualité Cas complexe fix

--- CreditImmo/CreditImmoTest/CalculTest.cs
+++ CreditImmo/CreditImmoTest/CalculTest.cs
@@ -6,17 +6,20 @@ namespace CreditImmoTest
     public class CalculTest
     {
         [Theory]
-        [InlineData(50000, 108, 1)]
-        public void CalculMensualiteTest(double montant, int duree, double taux)
+        [InlineData(50000, 108, 1, 484.3)]
+        [InlineData(50000, 300, 1, 188.44)]
+        [InlineData(100000,300, 10, 908.70)]
+
+        public void CalculMensualiteTest(double montant, int duree, double taux, double result )
         {
             Calcul calcul = new Calcul();
-            Assert.Equal(4167.4, CalculMensualite(montant, duree, taux));
+            Assert.Equal(result, CalculMensualite(montant, duree, taux));
         }
         public double CalculMensualite(double montant, int duree, double taux)
         {
             double mensualite;
-            double numerateur = montant * (taux / 12);
-            double denominateur = 1 - Math.Pow(1 + taux / 12, -duree);
+            double numerateur = montant * (taux/100 / 12);
+            double denominateur = 1 - (Math.Pow(1 + (taux/100 / 12), -duree));
             mensualite = numerateur / denominateur;
             mensualite = Math.Round(mensualite, 2);
             return mensualite;
