43a345d8a241caade673381c5e8e99cf5c4da441
Florian Spindler <florian.spindler@etu.unistra.fr>
2024-03-25 22:24:05 +0100

    CalculMensualité Refactor

--- CreditImmo/CreditImmo/Calcul.cs
+++ CreditImmo/CreditImmo/Calcul.cs
@@ -8,6 +8,15 @@ namespace CreditImmo
 {
     public class Calcul
     {
-        
+        public double CalculMensualite(double montant, int duree, double taux)
+        {
+            double mensualite;
+            double numerateur = montant * (taux / 100 / 12);
+            double denominateur = 1 - (Math.Pow(1 + (taux / 100 / 12), -duree));
+            mensualite = numerateur / denominateur;
+            mensualite = Math.Round(mensualite, 2);
+            return mensualite;
+        }
+
     }
 }
--- CreditImmo/CreditImmoTest/CalculTest.cs
+++ CreditImmo/CreditImmoTest/CalculTest.cs
@@ -13,16 +13,7 @@ namespace CreditImmoTest
         public void CalculMensualiteTest(double montant, int duree, double taux, double result )
         {
             Calcul calcul = new Calcul();
-            Assert.Equal(result, CalculMensualite(montant, duree, taux));
-        }
-        public double CalculMensualite(double montant, int duree, double taux)
-        {
-            double mensualite;
-            double numerateur = montant * (taux/100 / 12);
-            double denominateur = 1 - (Math.Pow(1 + (taux/100 / 12), -duree));
-            mensualite = numerateur / denominateur;
-            mensualite = Math.Round(mensualite, 2);
-            return mensualite;
+            Assert.Equal(result, calcul.CalculMensualite(montant, duree, taux));
         }
     }
 }
