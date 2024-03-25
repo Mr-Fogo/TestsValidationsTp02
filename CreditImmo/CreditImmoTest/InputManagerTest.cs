b93553cad63372ef1aa48a249554735e5434cd35
Florian Spindler <florian.spindler@etu.unistra.fr>
2024-03-25 20:42:11 +0100

    CheckDureeSimple

---
+++ CreditImmo/CreditImmoTest/InputManagerTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditImmoTest
+{
+    public class InputManagerTest
+    {
+        [Theory]
+        [InlineData(1)]
+        public void CheckDureeTestFalse(int duree)
+        {
+            Assert.False(CheckDuree(duree));
+        }
+
+        public static bool CheckDuree(int duree)
+        {
+           if (duree > 0)
+            {
+               return true;
+           }
+           else
+           {
+               return false;
+           }
+        }
+    }
+}
